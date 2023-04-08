60423b4569d6c0a05f534e310d3475df4b50e334
Rana <rana.kaushik@local.com>
2023-04-08 23:12:16 +1000

    wip

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,11 @@
 #this is to create VPC
 
 resource "aws_vpc" "main" {
-  cidr_block = var.VPC_CIDR
+  cidr_block           = var.VPC_CIDR
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "robot-${var.ENV}-VPC"
+  }
 }
 
